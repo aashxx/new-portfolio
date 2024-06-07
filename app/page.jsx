6d191ac85e7bcc1a04afcee307d78663d95ff671
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-06-07 23:37:36 +0530

    done

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,51 @@
+import Photo from '@/components/Photo';
+import Socials from '@/components/Socials';
+import Stats from '@/components/Stats';
+import { Button } from '@/components/ui/button';
 import React from 'react';
+import { FiDownload } from 'react-icons/fi';
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <main className='h-full'>
+      <section className="container mx-auto h-full">
+        <div className='flex flex-col-reverse lg:flex-row items-center justify-between lg:pt-8 lg:pb-24'>
+          <article className='text-center lg:text-left'>
+            <span className='text-xl'>
+              Software Developer
+            </span>
+            <h1 className='h1'>
+              Hello I'm <br />
+              <span className='text-accent'>
+                Mohamed Aashir
+              </span>
+            </h1>
+            <p className='max-w-[500px] mb-9 text-[gray] dark:text-white/80'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut consectetur, suscipit alias porro veniam.
+            </p>
+            <div className='flex flex-col lg:flex-row items-center gap-8'>
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>
+                  Download CV
+                </span>
+                <FiDownload className='text-xl' />
+              </Button>
+              <aside className='mb-8 lg:mb-0'>
+                <Socials containerStyles={'flex gap-6'} iconStyles={'w-9 h-9 border-accent rounded-full flex justify-center border items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500'} />
+              </aside>
+            </div>
+          </article>
+          <aside className='mb-8 lg:mb-0'>
+            <Photo />
+          </aside>
+        </div>
+      </section>
+      <section className='pt-4 pb-12 xl:pt-0 xl:pb-0'>
+        <div className="container mx-auto">
+          <Stats />
+        </div>
+      </section>
+    </main>
   )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,18 +6,18 @@ import MobileNav from './MobileNav';
 
 const Navbar = () => {
     return (
-        <nav className='py-8 xl:py-12'>
+        <nav className='py-8 lg:py-12'>
             <div className="container mx-auto flex justify-between items-center">
                 <Link href={'/'}>
                     <h1 className='text-4xl font-semibold'>
                         Aashir<span className='text-accent'>.</span>
                     </h1>
                 </Link>
-                <div className="hidden xl:flex items-center gap-8">
+                <div className="hidden lg:flex items-center gap-8">
                     <DesktopNav />
                     <ModeToggle />
                 </div>
-                <div className="xl:hidden">
+                <div className="lg:hidden">
                     <MobileNav />
                 </div>
             </div>
